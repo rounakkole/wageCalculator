d938c4066b173c89d889fb9b61657fe765bbc87a
rounak <rounakkole@gmail.com>
2022-02-08 14:38:11 +0530

    Add project files.

---
+++ wageCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+
+namespace wageCalculator;
+
+class Program
+{
+    public static void Main(String[] args)
+
+    {
+        Console.WriteLine($"Employee Wage Calculator");
+
+    }
+
+
+}
+
