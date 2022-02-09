21a9f92683769371b23a193d010a555c93a67fca
rounak <rounakkole@gmail.com>
2022-02-09 16:34:10 +0530

    ADDED: store daily wage into array

--- wageCalculator/Program.cs
+++ wageCalculator/Program.cs
@@ -8,58 +8,78 @@ namespace wageCalculator;
 class Program
 {
 
-    public static void GetEmpWage()
+    public static int GetEmpWage()
     {
+        //constant
+        const int ABSENT = 0;
+        const int PRESENT = 1;
+        const int PART_TIME = 2;
+        //declaration and initilization
         int workingHr = 0;
-        int perHrSalary = 20;
+
+
+
+
+        Random rnd = new Random();
+        int present = (rnd.Next(3));
+        switch (present)
+        {
+
+            case 0:
+                workingHr = ABSENT;
+                break;
+            case PRESENT:
+                workingHr = 8;
+                break;
+            case 2:
+                workingHr = PART_TIME;
+                break;
+            default:
+                Console.WriteLine("error");
+                break;
+        }
+
+        return workingHr;
+
+    }
+
+
+
+    public static void Main(String[] args)
+
+    {
+        Console.WriteLine($"Employee Wage Calculator");
+        //constant
+        const int PER_HR_SALARY = 20;
+        //declaration and initilization
         int totalSalary = 0;
         int totalHr = 0;
+        int[] dailyWageArray = new int[21];
 
         for (int day = 1; day <= 20; day++)
         {
-            Random rnd = new Random();
-            int present = (rnd.Next(3));
 
-            switch (present)
-            {
 
-                case 0:
-                    workingHr = 0;
-                    break;
-                case 1:
-                    workingHr = 8;
-                    break;
-                case 2:
-                    workingHr = 4;
-                    break;
-                default:
-                    Console.WriteLine("error");
-                    break;
-            }
-
-            int salary = perHrSalary * workingHr;
-            totalSalary = totalSalary + salary;
+            int workingHr=GetEmpWage();
 
             if (totalHr >= 100)
             {
                 break;
             }
+            int salary = PER_HR_SALARY * workingHr;
+            totalSalary = totalSalary + salary;
+            totalHr = totalHr + workingHr;
+
+            dailyWageArray[day] = salary;
 
         }
-        Console.WriteLine($"Total Salary: {totalSalary}");
-        Console.WriteLine($"Total working hours: {totalHr}");
+            Console.WriteLine($"Total Salary: {totalSalary}");
+            Console.WriteLine($"Total working hours: {totalHr}");
+            Console.WriteLine(String.Join(" ", dailyWageArray));
+        }
+
 
     }
-    public static void Main(String[] args)
-
-    {
-        Console.WriteLine($"Employee Wage Calculator");
-        GetEmpWage();
-
-    }
-
-
-}
 
 
 
