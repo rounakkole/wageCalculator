6bc849b8625ee648243bc0c6995694093e8e0388
rounak <rounakkole@gmail.com>
2022-02-10 10:03:22 +0530

    ADDED: store daily wage inside dictionary with day

--- wageCalculator/Program.cs
+++ wageCalculator/Program.cs
@@ -2,13 +2,60 @@
 
 
 using System;
+using System.Collections.Generic;
 
 namespace wageCalculator;
 
 class Program
 {
 
-    public static int GetEmpWage()
+    public static void Main(String[] args)
+
+    {
+        Console.WriteLine($"Employee Wage Calculator");
+        //constant
+        const int PER_HR_SALARY = 20;
+        //declaration and initilization
+        int totalSalary = 0;
+        int totalHr = 0;
+        int[] dailyWageArray = new int[20];
+        Dictionary<int, int> dailyWageDict = new Dictionary<int, int>();
+
+        for (int day = 0; day < 20; day++)
+        {
+
+
+            int workingHr = GetWorkingHr();
+
+            if (totalHr >= 100)
+            {
+                break;
+            }
+            int salary = PER_HR_SALARY * workingHr;
+            totalSalary = totalSalary + salary;
+            totalHr = totalHr + workingHr;
+
+            dailyWageArray[day] = salary;
+            dailyWageDict[day] = salary;
+            //dailyWageDict.Add(day, salary);
+
+        }
+        Console.WriteLine($"Total Salary: {totalSalary}");
+        Console.WriteLine($"Total working hours: {totalHr}");
+        //Console.WriteLine(String.Join(" ", dailyWageArray));
+
+        for (int i = 0; i < 20; i++)
+        {
+            //Console.WriteLine($"day {i}  wage {dailyWageArray[i]}");
+            Console.WriteLine($"day {i}  wage {dailyWageDict[i]}");
+
+        }
+
+    }
+
+
+
+    public static int GetWorkingHr()
     {
         //constant
         const int ABSENT = 0;
@@ -44,42 +91,7 @@ class Program
     }
 
 
-
-    public static void Main(String[] args)
-
-    {
-        Console.WriteLine($"Employee Wage Calculator");
-        //constant
-        const int PER_HR_SALARY = 20;
-        //declaration and initilization
-        int totalSalary = 0;
-        int totalHr = 0;
-        int[] dailyWageArray = new int[21];
-
-        for (int day = 1; day <= 20; day++)
-        {
-
-
-            int workingHr=GetEmpWage();
-
-            if (totalHr >= 100)
-            {
-                break;
-            }
-            int salary = PER_HR_SALARY * workingHr;
-            totalSalary = totalSalary + salary;
-            totalHr = totalHr + workingHr;
-
-            dailyWageArray[day] = salary;
-
-        }
-            Console.WriteLine($"Total Salary: {totalSalary}");
-            Console.WriteLine($"Total working hours: {totalHr}");
-            Console.WriteLine(String.Join(" ", dailyWageArray));
-        }
-
-
-    }
+}
 
 
 
